6e43dc2c45554a380de1fa0010b10cd239ea0425
MyslZhao <159505988+MyslZhao@users.noreply.github.com>
2026-07-23 20:31:20 +0800

    init

---
+++ src/main/java/top/myslzhao/mcbotchat/McBotChat.java
@@ -0,0 +1,16 @@
+package top.myslzhao.mcbotchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McBotChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
